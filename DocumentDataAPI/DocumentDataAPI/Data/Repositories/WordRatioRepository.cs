e38431dcb4ea352535b4a405b41728ff0ca18b42
KarmaKamikaze <nicolaijgn@gmail.com>
2022-10-13 13:01:52 +0200

    Fixed formatting and added namespace to exception

--- DocumentDataAPI/DocumentDataAPI/Data/Repositories/WordRatioRepository.cs
+++ DocumentDataAPI/DocumentDataAPI/Data/Repositories/WordRatioRepository.cs
@@ -1,112 +1,116 @@
 using System.Data;
 using Dapper;
+using DocumentDataAPI.Exceptions;
 using DocumentDataAPI.Models;
 
 namespace DocumentDataAPI.Data.Repositories;
 
 public class WordRatioRepository : IWordRatioRepository
 {
-  private readonly IDbConnectionFactory _connectionFactory;
+    private readonly IDbConnectionFactory _connectionFactory;
 
-  public WordRatioRepository(IDbConnectionFactory connectionFactory)
-  {
-    _connectionFactory = connectionFactory;
-  }
-
-  public int Add(WordRatioModel entity)
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.Execute(
-        "insert into word_ratios(documents_id, word, amount, percent, rank)" +
-        " values (@DocumentId, @Word, @Amount, @Percent, @Rank)",
-        new
-        {
-          entity.DocumentId,
-          entity.Word,
-          entity.Amount,
-          entity.Percent,
-          entity.Rank
-        });
-  }
-
-  public int AddWordRatios(IEnumerable<WordRatioModel> entities)
-  {
-    int rowsAffected = 0;
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    IDbTransaction transaction = con.BeginTransaction();
-    foreach (WordRatioModel entity in entities)
+    public WordRatioRepository(IDbConnectionFactory connectionFactory)
     {
-      rowsAffected += con.Execute(
-      "insert into word_ratios(documents_id, word, amount, percent, rank)" +
-      " values (@DocumentId, @Word, @Amount, @Percent, @Rank)",
-      new
-      {
-        entity.DocumentId,
-        entity.Word,
-        entity.Amount,
-        entity.Percent,
-        entity.Rank
-      }, transaction: transaction);
+        _connectionFactory = connectionFactory;
     }
-    if (rowsAffected != entities.Count())
+
+    public int Add(WordRatioModel entity)
     {
-      transaction.Rollback();
-      throw new RowsAffectedMismatchException();
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.Execute(
+            "insert into word_ratios(documents_id, word, amount, percent, rank)" +
+            " values (@DocumentId, @Word, @Amount, @Percent, @Rank)",
+            new
+            {
+                entity.DocumentId,
+                entity.Word,
+                entity.Amount,
+                entity.Percent,
+                entity.Rank
+            });
     }
-    transaction.Commit();
-    return rowsAffected;
-  }
 
-  public int Delete(WordRatioModel entity)
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.Execute("delete from word_ratios where documents_id=@DocumentId", new { entity.DocumentId });
-  }
-
-  public WordRatioModel Get(int id)
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.QuerySingle<WordRatioModel>("select * from word_ratios where id=@Id", new { id });
-  }
-
-  public IEnumerable<WordRatioModel> GetAll()
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.Query<WordRatioModel>($"select * from word_ratios");
-  }
-
-  public WordRatioModel GetByDocumentIdAndWord(int documentId, string word)
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.QuerySingle<WordRatioModel>(
-        "select * from word_ratios where word = @Word and documents_id = @Documentid",
-        new { DocumentId = documentId, Word = word });
-  }
-
-  public IEnumerable<WordRatioModel> GetByWord(string word)
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.Query<WordRatioModel>("select * from word_ratios where word = @Word", new { Word = word });
-  }
-
-  public IEnumerable<WordRatioModel> GetByWords(IEnumerable<string> wordlist)
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.Query<WordRatioModel>("select * from word_ratios where word = any(@wordlist)", new { wordlist });
-  }
-
-  public int Update(WordRatioModel entity)
-  {
-    using IDbConnection con = _connectionFactory.CreateConnection();
-    return con.Execute(
-        "update word_ratios set word = @Word, amount = @Amount, percent = @Percent, rank = @Rank where documents_id = @DocumentId",
-        new
+    public int AddWordRatios(IEnumerable<WordRatioModel> entities)
+    {
+        int rowsAffected = 0;
+        IEnumerable<WordRatioModel> wordRatios = entities.ToList();
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        IDbTransaction transaction = con.BeginTransaction();
+        foreach (WordRatioModel entity in wordRatios)
         {
-          entity.Word,
-          entity.Amount,
-          entity.Percent,
-          entity.Rank,
-          entity.DocumentId
-        });
-  }
+            rowsAffected += con.Execute(
+                "insert into word_ratios(documents_id, word, amount, percent, rank)" +
+                " values (@DocumentId, @Word, @Amount, @Percent, @Rank)",
+                new
+                {
+                    entity.DocumentId,
+                    entity.Word,
+                    entity.Amount,
+                    entity.Percent,
+                    entity.Rank
+                }, transaction: transaction);
+        }
+
+        if (rowsAffected != wordRatios.Count())
+        {
+            transaction.Rollback();
+            throw new RowsAffectedMismatchException();
+        }
+
+        transaction.Commit();
+        return rowsAffected;
+    }
+
+    public int Delete(WordRatioModel entity)
+    {
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.Execute("delete from word_ratios where documents_id=@DocumentId", new { entity.DocumentId });
+    }
+
+    public WordRatioModel Get(int id)
+    {
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.QuerySingle<WordRatioModel>("select * from word_ratios where id=@Id", new { id });
+    }
+
+    public IEnumerable<WordRatioModel> GetAll()
+    {
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.Query<WordRatioModel>($"select * from word_ratios");
+    }
+
+    public WordRatioModel GetByDocumentIdAndWord(int documentId, string word)
+    {
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.QuerySingle<WordRatioModel>(
+            "select * from word_ratios where word = @Word and documents_id = @Documentid",
+            new { DocumentId = documentId, Word = word });
+    }
+
+    public IEnumerable<WordRatioModel> GetByWord(string word)
+    {
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.Query<WordRatioModel>("select * from word_ratios where word = @Word", new { Word = word });
+    }
+
+    public IEnumerable<WordRatioModel> GetByWords(IEnumerable<string> wordlist)
+    {
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.Query<WordRatioModel>("select * from word_ratios where word = any(@wordlist)", new { wordlist });
+    }
+
+    public int Update(WordRatioModel entity)
+    {
+        using IDbConnection con = _connectionFactory.CreateConnection();
+        return con.Execute(
+            "update word_ratios set word = @Word, amount = @Amount, percent = @Percent, rank = @Rank where documents_id = @DocumentId",
+            new
+            {
+                entity.Word,
+                entity.Amount,
+                entity.Percent,
+                entity.Rank,
+                entity.DocumentId
+            });
+    }
 }
--- DocumentDataAPI/DocumentDataAPI/Exceptions/RowsAffectedMismatch.cs
+++ DocumentDataAPI/DocumentDataAPI/Exceptions/RowsAffectedMismatch.cs
@@ -1,12 +1,27 @@
+namespace DocumentDataAPI.Exceptions;
 
-//This exception will be used when a transaction needs to rollback, since
-[System.Serializable]
-public class RowsAffectedMismatchException : System.Exception
+/// <summary>
+/// This exception will be used when a transaction needs to rollback, since the rows affected does not match the amount
+/// of entities that should have been affected in the database due to the query.
+/// </summary>
+[Serializable]
+public class RowsAffectedMismatchException : Exception
 {
-  public RowsAffectedMismatchException() { }
-  public RowsAffectedMismatchException(string message) : base(message) { }
-  public RowsAffectedMismatchException(string message, System.Exception inner) : base(message, inner) { }
-  protected RowsAffectedMismatchException(
-    System.Runtime.Serialization.SerializationInfo info,
-    System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    public RowsAffectedMismatchException()
+    {
+    }
+
+    public RowsAffectedMismatchException(string message) : base(message)
+    {
+    }
+
+    public RowsAffectedMismatchException(string message, Exception inner) : base(message, inner)
+    {
+    }
+
+    protected RowsAffectedMismatchException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context)
+    {
+    }
 }
