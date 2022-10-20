27adda47c008cfb81177718e35e7bb30c66d9d5f
Nikolaj Dam <ndam20@student.aau.dk>
2022-10-20 16:54:52 +0200

    Switch to file-scoped namespace

--- DocumentDataAPI/DocumentDataAPI/Controllers/DocumentController.cs
+++ DocumentDataAPI/DocumentDataAPI/Controllers/DocumentController.cs
@@ -5,128 +5,127 @@ using Microsoft.AspNetCore.Mvc;
 
 using System.Net.Mime;
 
-namespace DocumentDataAPI.Controllers
+namespace DocumentDataAPI.Controllers;
+
+[ApiController]
+[Route(RoutePrefixHelper.Prefix + "/documents")]
+[Produces(MediaTypeNames.Application.Json)]
+public class DocumentController : ControllerBase
 {
-    [ApiController]
-    [Route(RoutePrefixHelper.Prefix + "/documents")]
-    [Produces(MediaTypeNames.Application.Json)]
-    public class DocumentController : ControllerBase
+    private readonly ILogger<DocumentController> _logger;
+    private readonly IDocumentRepository _repository;
+
+    public DocumentController(ILogger<DocumentController> logger, IDocumentRepository repository)
     {
-        private readonly ILogger<DocumentController> _logger;
-        private readonly IDocumentRepository _repository;
+        _logger = logger;
+        _repository = repository;
+    }
 
-        public DocumentController(ILogger<DocumentController> logger, IDocumentRepository repository)
+    /// <summary>
+    /// Adds the document from the content body.
+    /// </summary>
+    /// <response code="200">Success: The document that was added to the database.</response>
+    /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<DocumentModel> PutDocument([FromBody] List<DocumentModel> documents)
+    {
+        try
         {
-            _logger = logger;
-            _repository = repository;
+            return _repository.AddBatch(documents) == 0
+                ? Problem("No rows were added")
+                : Ok($"Added {documents.Count} documents.");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Unable to add document.");
+            return Problem(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// Retrieves a list of all documents from the database.
+    /// </summary>
+    /// <response code="200">Success: A list of all documents</response>
+    /// <response code="404">Not Found: Nothing is returned.</response>
+    /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<IEnumerable<DocumentModel>> GetAll(int? sourceId, string? author, DateTime? beforeDate, DateTime? afterDate)
+    {
+        try
+        {
+            DocumentSearchParameters parameters = new DocumentSearchParameters();
+            if (sourceId is not null) parameters.AddSource(sourceId.Value);
+            if (author is not null) parameters.AddAuthor(author);
+            if (beforeDate is not null) parameters.AddBeforeDate(beforeDate.Value);
+            if (afterDate is not null) parameters.AddAfterDate(afterDate.Value);
+
+            IEnumerable<DocumentModel> result = _repository.GetAll(parameters);
+            return result.Any()
+                ? NotFound()
+                : Ok(result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Unable to get documents.");
+            return Problem(e.Message);
         }
 
-        /// <summary>
-        /// Adds the document from the content body.
-        /// </summary>
-        /// <response code="200">Success: The document that was added to the database.</response>
-        /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
-        [HttpPut]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<DocumentModel> PutDocument([FromBody] List<DocumentModel> documents)
+    }
+
+    /// <summary>
+    /// Retrieves a document based on the document id.
+    /// </summary>
+    /// <response code="200">Success: A document for the given id.</response>
+    /// <response code="404">Not Found: Nothing is returned.</response>
+    /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
+    [HttpGet]
+    [Route("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<DocumentModel> GetById(int id)
+    {
+        try
         {
-            try
-            {
-                return _repository.AddBatch(documents) == 0
-                    ? Problem("No rows were added")
-                    : Ok($"Added {documents.Count} documents.");
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Unable to add document.");
-                return Problem(e.Message);
-            }
+            DocumentModel? result = _repository.Get(id);
+            return result == null
+                ? NotFound()
+                : Ok(result);
         }
-
-        /// <summary>
-        /// Retrieves a list of all documents from the database.
-        /// </summary>
-        /// <response code="200">Success: A list of all documents</response>
-        /// <response code="404">Not Found: Nothing is returned.</response>
-        /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<IEnumerable<DocumentModel>> GetAll(int? sourceId, string? author, DateTime? beforeDate, DateTime? afterDate)
+        catch (Exception e)
         {
-            try
-            {
-                DocumentSearchParameters parameters = new DocumentSearchParameters();
-                if (sourceId is not null) parameters.AddSource(sourceId.Value);
-                if (author is not null) parameters.AddAuthor(author);
-                if (beforeDate is not null) parameters.AddBeforeDate(beforeDate.Value);
-                if (afterDate is not null) parameters.AddAfterDate(afterDate.Value);
-
-                IEnumerable<DocumentModel> result = _repository.GetAll(parameters);
-                return result.Any()
-                    ? NotFound()
-                    : Ok(result);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Unable to get documents.");
-                return Problem(e.Message);
-            }
-
+            _logger.LogError(e, "Unable to get document with id: {id}.", id);
+            return Problem(e.Message);
         }
+    }
 
-        /// <summary>
-        /// Retrieves a document based on the document id.
-        /// </summary>
-        /// <response code="200">Success: A document for the given id.</response>
-        /// <response code="404">Not Found: Nothing is returned.</response>
-        /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
-        [HttpGet]
-        [Route("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<DocumentModel> GetById(int id)
+    /// <summary>
+    /// Retrieves the number of documents in the database.
+    /// </summary>
+    /// <response code="200">Success: A number</response>
+    /// <response code="404">Not Found: Nothing is returned.</response>
+    /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
+    [HttpGet]
+    [Route("count")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<int> GetTotalDocumentCount()
+    {
+        try
         {
-            try
-            {
-                DocumentModel? result = _repository.Get(id);
-                return result == null
-                    ? NotFound()
-                    : Ok(result);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Unable to get document with id: {id}.", id);
-                return Problem(e.Message);
-            }
+            int result = _repository.GetTotalDocumentCount();
+            return Ok(result);
         }
-
-        /// <summary>
-        /// Retrieves the number of documents in the database.
-        /// </summary>
-        /// <response code="200">Success: A number</response>
-        /// <response code="404">Not Found: Nothing is returned.</response>
-        /// <response code="500">Internal Server Error: a <see cref="ProblemDetails"/> describing the error.</response>
-        [HttpGet]
-        [Route("count")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<int> GetTotalDocumentCount()
+        catch (Exception e)
         {
-            try
-            {
-                int result = _repository.GetTotalDocumentCount();
-                return Ok(result);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Unable to get document count.");
-                return Problem(e.Message);
-            }
+            _logger.LogError(e, "Unable to get document count.");
+            return Problem(e.Message);
         }
     }
 }
