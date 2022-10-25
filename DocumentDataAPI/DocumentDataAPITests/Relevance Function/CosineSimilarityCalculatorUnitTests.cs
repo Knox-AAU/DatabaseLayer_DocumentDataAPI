be828ba1e065240ed062c4ae877c81934c6271eb
Sarmisuper <sarmilan3@gmail.com>
2022-10-25 15:30:14 +0200

    More unit tests, testing when words in the query are not present in the document

--- DocumentDataAPI/DocumentDataAPITests/Relevance Function/CosineSimilarityCalculatorUnitTests.cs
+++ DocumentDataAPI/DocumentDataAPITests/Relevance Function/CosineSimilarityCalculatorUnitTests.cs
@@ -54,6 +54,16 @@ public class CosineSimilarityCalculatorUnitTests
                     {"test3", 10}}, 
                 new List<string>()
                 {
+                    "test2", "test10"
+                }, 
+                5/(Math.Sqrt(Math.Pow(1,2)+Math.Pow(5,2)+Math.Pow(10,2)) * Math.Sqrt(2))},
+            new object[] { 
+                new Dictionary<string, int>() {
+                    {"test1", 1},
+                    {"test2", 5},
+                    {"test3", 10}}, 
+                new List<string>()
+                {
                     "test3"
                 }, 
                 10/Math.Sqrt(Math.Pow(1,2)+Math.Pow(5,2)+Math.Pow(10,2))},
@@ -79,6 +89,19 @@ public class CosineSimilarityCalculatorUnitTests
                     "test3", "test4"
                 }, 
                 35/(Math.Sqrt(Math.Pow(10,2)+Math.Pow(15,2)+Math.Pow(10,2)+Math.Pow(25,2)+Math.Pow(20,2)) * Math.Sqrt(2))},
+            new object[] { 
+                new Dictionary<string, int>() {
+                    {"test1", 10},
+                    {"test2", 15},
+                    {"test3", 10},
+                    {"test4", 25},
+                    {"test5", 20}},
+                new List<string>()
+                {
+                    "test3", "test4", "test10", "test11", "test12"
+                }, 
+                35/(Math.Sqrt(Math.Pow(10,2)+Math.Pow(15,2)+Math.Pow(10,2)+Math.Pow(25,2)+Math.Pow(20,2)) * Math.Sqrt(5))},
+
         };
         
     
