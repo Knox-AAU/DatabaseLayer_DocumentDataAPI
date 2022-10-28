bb1d11a259deae955ccf97978375cae3d6be05bf
Nikolaj Dam <ndam20@student.aau.dk>
2022-10-28 21:13:34 +0200

    More deploy_schema.sql fixerinos

--- DocumentDataAPI/DocumentDataAPI/Data/Deployment/Scripts/deploy_schema.sql
+++ DocumentDataAPI/DocumentDataAPI/Data/Deployment/Scripts/deploy_schema.sql
@@ -1,29 +1,28 @@
 -- Creates a copy of the document_data schema (see https://wiki.knox.cs.aau.dk/en/Database/DocumentDataAPI/document_data).
 -- The actual name of the schema is provided though the ${schema} placeholder.
--- Also creates the readonly and read_write users that are used in production (but not necessarily during local development)
-
+-- Also creates the readonly and read_write users that are used in production.
 drop schema if exists ${schema} cascade;
 
 create schema ${schema};
 
 create table ${schema}.data_sources (
-    id          bigint generated always as identity primary key,
-    name        varchar(100) not null
+    id     bigint generated always as identity primary key,
+    name   varchar(100) not null
 );
 
 create index data_sources_name_idx on ${schema}.data_sources (name);
 
 create table ${schema}.categories (
-    category_id     integer generated always as identity primary key,
-    name            varchar(100)
+    id     integer generated always as identity primary key,
+    name   varchar(100) not null
 );
 
 create index categories_name_idx on ${schema}.categories (name);
 
 create table ${schema}.documents (
     id              bigint primary key,
-    sources_id      bigint not null,
-    categories_id   bigint not null,
+    data_sources_id bigint not null references ${schema}.data_sources(id),
+    categories_id   bigint not null references ${schema}.categories(id),
     publication     varchar(100),
     title           varchar(400) not null,
     path            varchar(400) not null,
@@ -31,36 +30,32 @@ create table ${schema}.documents (
     date            timestamp not null,
     author          varchar(200) not null,
     total_words     bigint not null,
-    unique_words    bigint not null,
-    constraint fk_data_sources foreign key (sources_id) references ${schema}.data_sources(id),
-    constraint fk_categories foreign key (categories_id) references ${schema}.categories
+    unique_words    bigint not null
 );
 
-create index documents_sources_id_idx on ${schema}.documents (sources_id);
+create index documents_data_sources_id_idx on ${schema}.documents (data_sources_id);
 create index documents_categories_id_idx on ${schema}.documents (categories_id);
 create index documents_author_idx on ${schema}.documents (author);
 create index documents_date_idx on ${schema}.documents (date);
 create index documents_publication_idx on ${schema}.documents (publication);
 
 create table ${schema}.word_ratios (
-    documents_id        bigint  not null,
+    documents_id        bigint not null references ${schema}.documents(id),
     word                varchar(100) not null,
     amount              integer not null,
     percent             real not null,
     rank                integer not null,
     clustering_score    float default 0 not null,
-    constraint fk_documents foreign key (documents_id) references ${schema}.documents(id),
     constraint pk_files_id_word primary key (word, documents_id)
 );
 
 create index word_ratios_documents_id_idx on ${schema}.word_ratios (documents_id);
 
 create table ${schema}.document_contents (
-    documents_id    bigint not null,
+    documents_id    bigint not null references ${schema}.documents(id),
     index           integer not null,
     subheading      varchar(500),
     content         text not null,
-    constraint fk_documents foreign key (documents_id) references ${schema}.documents(id),
     constraint pk_document_contents primary key (documents_id, index)
 );
 
