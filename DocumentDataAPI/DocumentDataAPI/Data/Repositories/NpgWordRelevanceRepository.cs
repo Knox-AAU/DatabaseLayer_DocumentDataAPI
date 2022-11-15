05644fbb91d10d1229b8c7237eeeaa6eade12af3
Nikolaj Dam <ndam20@student.aau.dk>
2022-11-15 14:17:50 +0100

    Changed "select into" to "create temp table as"

--- DocumentDataAPI/DocumentDataAPI/Data/Repositories/NpgWordRelevanceRepository.cs
+++ DocumentDataAPI/DocumentDataAPI/Data/Repositories/NpgWordRelevanceRepository.cs
@@ -21,24 +21,26 @@ public class NpgWordRelevanceRepository : IWordRelevanceRepository
         _logger.LogInformation("Updating all TF-IDF scores in database");
         // Create temporary table with the IDF value of each distinct word
         // Then set TF-IDF of each word_ratio to the TF of the word (Percent) times the IDF value for the word.
-        const string script = $@"drop table if exists temp_idf_values;
-select q1.word, ln(q2.total_documents::decimal / q1.documents_for_word) idf
-into temp_idf_values
-from (
-    select {WordRatioMap.Word}, count(distinct {WordRatioMap.DocumentId}) documents_for_word
-    from word_ratios
-    group by {WordRatioMap.Word}
-)q1
-join (
-    select count(1) total_documents
-    from documents
-)q2 on 1=1;
+        const string updateSql = $@"
+create temp table temp_idf_values as 
+    select q1.word, ln(q2.total_documents::decimal / q1.documents_for_word) idf
+    from (
+        select {WordRatioMap.Word}, count(distinct {WordRatioMap.DocumentId}) documents_for_word
+        from word_ratios
+        group by {WordRatioMap.Word}
+    )q1
+    join (
+        select count(1) total_documents
+        from documents
+    )q2 on 1=1;
+
 update word_ratios wr
     set {WordRatioMap.TfIdf} = {WordRatioMap.Percent} * temp.idf
-from temp_tf_idf_values temp
-    where wr.{WordRatioMap.Word} = temp.word";
-            
+    from temp_idf_values temp
+    where wr.{WordRatioMap.Word} = temp.word;
+    ";
+        
         using IDbConnection con = _connectionFactory.CreateConnection();
-        return await con.ExecuteAsync(script, commandTimeout: 0);
+        return await con.ExecuteAsync(updateSql, commandTimeout: 0);
     }
 }
