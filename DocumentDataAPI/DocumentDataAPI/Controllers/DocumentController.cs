b473f21e9fedde11c495fb3be2aeed35d5a81bae
mirakst <mstids20@student.aau.dk>
2022-11-18 11:35:55 +0100

    Naive attempt at pleasing the linter

--- DocumentDataAPI/DocumentDataAPI/Controllers/DocumentController.cs
+++ DocumentDataAPI/DocumentDataAPI/Controllers/DocumentController.cs
@@ -59,8 +59,7 @@ public class DocumentController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<IEnumerable<DocumentModel>>> GetAll([FromQuery]List<long> sourceIds, [FromQuery]List<string> authors, [FromQuery]List<int> categoryIds, DateTime? beforeDate, DateTime? afterDate, int? limit, int? offset)
-
+    public async Task<ActionResult<IEnumerable<DocumentModel>>> GetAll([FromQuery] List<long> sourceIds, [FromQuery] List<string> authors, [FromQuery] List<int> categoryIds, DateTime? beforeDate, DateTime? afterDate, int? limit, int? offset)
     {
         try
         {
